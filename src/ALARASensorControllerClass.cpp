7f47fa6d2decf5aea2e00705771c6cc0b96b121e
Daniel Morgan <dmorgan.628@gmail.com>
2022-05-31 13:30:37 -0700

    fixed my bad syntax ALARAV2SensorController

--- src/ALARASensorControllerClass.cpp
+++ src/ALARASensorControllerClass.cpp
@@ -1,7 +1,7 @@
 #include "ALARASensorControllerClass.h"
 #include <Arduino.h>
 
-ALARAV2SensorController::ALARAV2SensorController(uint32_t controllerID, uint8_t setControllerNodeID, bool setBNO055_present, bool BMI085_present, bool KX134_1211_present, bool SAM_M8Q_GPS_present, bool MS5607_present, bool setNodeIDCheck)
+ALARAV2SensorController::ALARAV2SensorController(uint32_t setControllerID, uint8_t setControllerNodeID, bool setBNO055_present, bool setBMI085_present, bool setKX134_1211_present, bool setSAM_M8Q_GPS_present, bool setMS5607_present, bool setNodeIDCheck)
                          : controllerID{setControllerID}, controllerNodeID{setControllerNodeID}, BNO055_present{setBNO055_present}, BMI085_present{setBMI085_present}, KX134_1211_present{setKX134_1211_present}, SAM_M8Q_GPS_present{setSAM_M8Q_GPS_present}, MS5607_present{setMS5607_present}, nodeIDCheck{setNodeIDCheck}
 {
     // Instantiation stuff?
